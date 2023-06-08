75159c624ee38e9400253d87b308f87faf698ae0
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2023-06-08 21:22:07 +1000

    Cleanup

--- ScrollingCombatTextParentMapper_Patch.cs
+++ ScrollingCombatTextParentMapper_Patch.cs
@@ -1,73 +1,70 @@
 ﻿using System;
-using System.Text;
 using HarmonyLib;
 using ProjectM;
 using ProjectM.UI;
 using Unity.Collections;
 using Unity.Entities;
 
-namespace PopupTotals
+namespace PopupTotals;
+
+[HarmonyPatch]
+public class ScrollingCombatTextParentMapperPatch
 {
-    [HarmonyPatch]
-    public class ScrollingCombatTextParentMapperPatch
+    private static Action<SCTText, ScrollingCombatTextParentMapper.EntryData> _handler;
+
+    private static bool _hooked;
+    private static PrefabGUID? _sctTypeResourceGain;
+    private static World _world;
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(ScrollingCombatTextParentMapper), nameof(ScrollingCombatTextParentMapper.OnUpdate))]
+    private static void OnUpdate_Prefix(ref ScrollingCombatTextParentMapper __instance)
     {
-        private static bool _hooked;
-        private static PrefabGUID? _sctTypeResourceGain;
-        private static World _world;
-
-        [HarmonyPrefix]
-        [HarmonyPatch(typeof(ScrollingCombatTextParentMapper), nameof(ScrollingCombatTextParentMapper.OnUpdate))]
-        private static void OnUpdate_Prefix(ref ScrollingCombatTextParentMapper __instance)
+        if (_hooked) return;
+        var pcs = __instance.World.GetExistingSystem<PrefabCollectionSystem>();
+        if (pcs == null || __instance._Elements == null)
         {
-            if (_hooked) return;
-            var pcs = __instance.World.GetExistingSystem<PrefabCollectionSystem>();
-            if (pcs == null || __instance._Elements == null)
-            {
-                _hooked = false;
-                return;
-            }
-
-            var gain = pcs.NameToPrefabGuidDictionary.TryGetValue("(SCTType) SCT_Type_ResouceGain", out var guid);
-            if (gain)
-            {
-                _sctTypeResourceGain = guid;
-            }
-
-            if (_sctTypeResourceGain == null)
-            {
-                _hooked = false;
-                return;
-            }
-
-            _world = __instance.World;
-            __instance._Elements.OnEntryUpdate += new Action<SCTText, ScrollingCombatTextParentMapper.EntryData>(OnEntryUpdate);
-            _hooked = true;
-        }
-
-        private static void OnEntryUpdate(SCTText text, ScrollingCombatTextParentMapper.EntryData entry)
-        {
-            if (_sctTypeResourceGain != null && entry.Type.GuidHash != _sctTypeResourceGain.Value.GuidHash) return;
-            var before = entry.SourceTypeText.ToString();
-            var str = entry.SourceTypeText.ToString();
-            if (str.Contains('(') && str.Contains(')')) return;
-            var sb = new StringBuilder(str);
-            var prefab = ItemUtils.GetOrRebuild(entry.SourceTypeText.ToString());
-            ConsoleShared.TryGetLocalCharacter(out var character, _world);
-            var total = InventoryUtilities.GetItemAmount(_world.EntityManager,
-                character, prefab);
-            sb.Append(" (");
-            sb.Append(total);
-            sb.Append(')');
-            entry.SourceTypeText = new FixedString128(sb.ToString());
-            text.Text.m_text = text.Text.m_text.Replace(before, sb.ToString());
-        }
-
-        [HarmonyPostfix]
-        [HarmonyPatch(typeof(ScrollingCombatTextParentMapper), nameof(ScrollingCombatTextParentMapper.OnDestroy))]
-        private static void OnDestroy_Postfix(ref ScrollingCombatTextParentMapper __instance)
-        {
-            __instance._Elements.OnEntryUpdate -= new Action<SCTText, ScrollingCombatTextParentMapper.EntryData>(OnEntryUpdate);
             _hooked = false;
+            return;
         }
+
+        var gain = pcs.NameToPrefabGuidDictionary.TryGetValue("(SCTType) SCT_Type_ResouceGain", out var guid);
+        if (gain)
+        {
+            _sctTypeResourceGain = guid;
+        }
+
+        if (_sctTypeResourceGain == null)
+        {
+            _hooked = false;
+            return;
+        }
+
+        _world = __instance.World;
+        _handler = OnEntryUpdate;
+        __instance._Elements.OnEntryUpdate += _handler;
+        _hooked = true;
+    }
+
+    private static void OnEntryUpdate(SCTText text, ScrollingCombatTextParentMapper.EntryData entry)
+    {
+        if (_sctTypeResourceGain != null && entry.Type.GuidHash != _sctTypeResourceGain.Value.GuidHash) return;
+        var str = entry.SourceTypeText.ToString();
+        if (str.Contains('(') && str.Contains(')')) return;
+        var prefab = ItemUtils.GetOrRebuild(str);
+        ConsoleShared.TryGetLocalCharacter(out var character, _world);
+        var total = InventoryUtilities.GetItemAmount(_world.EntityManager, character, prefab);
+        var newStr = $"{str} ({total})";
+        entry.SourceTypeText = new FixedString128(newStr);
+        text.Text.m_text = text.Text.m_text.Replace(str, newStr);
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(ScrollingCombatTextParentMapper), nameof(ScrollingCombatTextParentMapper.OnDestroy))]
+    private static void OnDestroy_Postfix(ref ScrollingCombatTextParentMapper __instance)
+    {
+        if (_handler == null) return;
+        __instance._Elements.OnEntryUpdate -= _handler;
+        _hooked = false;
     }
 }
